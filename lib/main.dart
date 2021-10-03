03a58778ab1b22caeb84f53c0718c55836bf6410
Ole Timmers <s1115222@studen.hsleiden.nl>
2021-10-03 15:02:03 +0200

    Step 2 to step 6 finished.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -15,10 +16,37 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue.shade900,
         title: Text('Ask me anything'),
       ),
-      body: Container(
-
-      ),
+      body: Ball(),
     );
   }
+}
 
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballID = 1;
+
+  void askQuestion() {
+    setState(() {
+      ballID = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FlatButton(
+            onPressed: () {
+              askQuestion();
+            },
+            child: Image.asset('images/ball$ballID.png')
+        ),
+      ],
+    );
+  }
 }
